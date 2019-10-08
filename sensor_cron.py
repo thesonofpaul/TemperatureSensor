9f8d8b5d9c12d582f740047928b4489e39203bb3
Zach P <zip822@gmail.com>
2019-10-07 22:08:04 -0700

    debug messages

--- sensor_cron.py
+++ sensor_cron.py
@@ -14,6 +14,8 @@ def read_sensor():
             temperature_f = 9.0/5.0 * temperature_c + 32
             print("Temp: {0:0.1f} F  Humidity: {1:0.1f} %".format(temperature_f, humidity))
             successful_reading = True
+        else:
+            print("unsuccessful")
     return temperature_f, humidity
 
 if __name__ == '__main__':
