1bdb8c7d821d295133361380839271cfb70603ce
Zach Paulson <zip822@gmail.com>
2019-12-10 12:05:56 -0800

    Update sms_manager.py

--- sms_manager.py
+++ sms_manager.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 class SMS:
     def __init__(self):
-        self.account_sid = 'ACf233e286344d8981d9a92908aa5bd9de'
-        self.auth_token = 'e20900566df288ba4b002ebeb50e21bf'
+        self.account_sid = ''
+        self.auth_token = ''
 
     def _create_client(self):
         return Client(self.account_sid, self.auth_token)
@@ -12,10 +12,10 @@ class SMS:
         client = self._create_client()
         message = client.messages.create(
             to=recipient, 
-            from_="+13342265224",
+            from_="",
             body="Hi Bearington G! I sent this through twilio!")
         print(message.sid)
 
 if __name__ == '__main__':
     sms = SMS()
-    sms.send_sms('+16037699678')
+    sms.send_sms('')
